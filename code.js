2b80b5f3ba7de1438e40c70908cc9d4573a32065
Juanksyek <elgalleto12393@gmail.com>
2023-03-22 17:09:30 -0600

    Se han solucionado problemas con el dise;o responsive, se agregó una nueva vista (01) y sus respectivos estilos.

--- code.js
+++ code.js
@@ -1,8 +1,13 @@
-var toggler = document.querySelector('div.toggle');
-var navigation = document.querySelector('navigation');
+var toggler = document.querySelector("div.toggle");
+var navigation = document.querySelector("navigation");
+var backdrop = document.querySelector(".backdrop");
 
-toggler.addEventListener('click', (e) => {
-  e.target.classList.toggle('fa-bars')
-  e.target.classList.toggle('fa-times')
-  navigation.classList.toggle('menu-close') 
+toggler.addEventListener("click", (e) => {
+    e.target.classList.toggle("fa-bars");
+    e.target.classList.toggle("fa-times");
+    navigation.classList.toggle("menu-close");
+});
+
+backdrop.addEventListener("click", (e) => {
+    navigation.classList.remove("menu-close");
 });
